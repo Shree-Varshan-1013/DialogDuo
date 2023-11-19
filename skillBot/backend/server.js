d908e3c53c4bcce0248e9d756e860bf187dad9b5
Shree Varshan G <shreevarshang2003@gmail.com>
2023-11-19 20:44:29 +0530

    small

--- skillBot/backend/server.js
+++ skillBot/backend/server.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-
 const replicate = new Replicate({
     auth: "r8_BKNtsbMFJVy7iRYQtYAoWqLyauFVUF50eUM38",
 });
@@ -28,24 +27,6 @@ const main = async() => {
 }
 
 main();
-// const replicate = new Replicate({
-//     auth: "r8_BKNtsbMFJVy7iRYQtYAoWqLyauFVUF50eUM38",
-// });
-
-// const model = "andreasjansson/blip-2:4b32258c42e9efd4288bb9910bc532a69727f9acd26aa08e175713a0a857a608";
-
-// const input = {
-//     image: "https://replicate.delivery/pbxt/IJEPmgAlL2zNBNDoRRKFegTEcxnlRhoQxlNjPHSZEy0pSIKn/gg_bridge.jpeg",
-//     question: "what is the color of bridge?"
-// };
-
-// const get = async () => {
-//     const output = await replicate.run(model, { input });
-//     console.log(output);
-// }
-
-// Output:
-// san francisco bay
 
 app.get('/', (req, res) => {
     res.send('Hello World')
