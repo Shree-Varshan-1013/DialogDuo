2bca3b933084bc74f29583ecc37e1d3bb5ee8729
Samyukta Karthikeyan <727721eucs129@skcet.ac.in>
2023-11-20 12:56:20 +0530

    images color change

--- skillBot/frontend/src/main.jsx
+++ skillBot/frontend/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx';
 import './styles/index.css'
 import Home from './components/Home.jsx';
+
 import ChatBot from './components/ChatBot.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChatBot />
+    <App/>
   </React.StrictMode>,
 )
